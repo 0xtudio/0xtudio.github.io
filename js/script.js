de29dbe5f9414d9b60a5d23aeca2c416fc573613
Saeed Khatami <0xb4dc0d3x@gmail.com>
2023-12-20 03:21:58 +0330

    slider

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const modeSwitch = document.getElementById('modeSwitch');
+    const moonIcon = document.querySelector('.moon');
+    const sunIcon = document.querySelector('.sun');
 
     modeSwitch.addEventListener('change', function () {
         document.body.classList.toggle('dark-mode');
+
+        if (document.body.classList.contains('dark-mode')) {
+            moonIcon.style.transform = 'translate(-50%, -50%) scale(0)';
+            sunIcon.style.transform = 'translate(-50%, -50%) scale(1)';
+        } else {
+            moonIcon.style.transform = 'translate(-50%, -50%) scale(1)';
+            sunIcon.style.transform = 'translate(-50%, -50%) scale(0)';
+        }
     });
 });
